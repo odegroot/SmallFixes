1f281d1a0588d0d741c27ad54654a84952b231b4
Oscar de Groot <odegroot@gmail.com>
2016-11-07 23:57:12 +0100

    Strip out everything, but keep making logistic bots unminable

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,56 +1,10 @@
-require "config"
-require "util"
+-- Make logistic bots unminable.
 
-local types = {"construction-robot", "logistic-robot"}
-local bots = {}
-for _, bot in pairs(types) do
-  for _, e in pairs(data.raw[bot]) do
-    bots[e.name] = true
-
-    if config.mine_bot_materials then
-      if data.raw["recipe"][e.name] and data.raw["recipe"][e.name].result == e.name then
-        e.minable.results = util.table.deepcopy(data.raw["recipe"][e.name].ingredients)
-        e.minable.result = nil
-      else
-        for _, r in pairs(data.raw["recipe"]) do
-          if r.result == e.name then
-            e.minable.results = util.table.deepcopy(r.ingredients)
-            e.minable.result = nil
-            break
-          end
-        end
-      end
-    end
-
-    if bot == "construction-robot" and config.unminable_bots then
-      e.minable = nil
-    end
-    
-    if bot == "logistic-robot" and config.unminable_logistic_bots then
-      e.minable = nil
-    end
-  end
-end
-
-if config.fix_bots then
-  for _, e in pairs(data.raw["item"]) do
-    if e.place_result and bots[e.place_result] then
-      local r = false
-      for i,f in pairs(e.flags) do
-        if f == "goes-to-quickbar" then
-          r = i
-          break
-        end
-      end
-      if r then
-        table.remove(e.flags, r)
-      end
-    end
-  end
-end
-
-if config.fix_requester_paste_multiplier then
-  for _, r in pairs(data.raw["recipe"]) do
-    r.requester_paste_multiplier = config.requester_paste_multiplier
-  end
+-- In vanilla Factorio, there is only one type of logistic robot, named "logistic-robot".
+-- So for vanilla the for-loop could be skipped and we could just do the following.
+--     data.raw["logistic-robot"]["logistic-robot"].minable = false
+-- However, some mods add more types of logistic robots.
+-- With this for-loop, we just make them *all* unminable, regardless of how many there are.
+for name, entity in pairs(data.raw["logistic-robot"]) do
+  entity.minable = nil
 end
--- migrations/SmallFixes_0.0.1.lua
+++
@@ -1,8 +0,0 @@
-for _, player in pairs(game.players) do
-  player.force.reset_recipes()
-  player.force.reset_technologies()
-
-  if player.force.technologies["advanced-electronics"].researched and player.force.recipes["radar-mk2"] then
-    player.force.recipes["radar-mk2"].enabled = true
-  end
-end
